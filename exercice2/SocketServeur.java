6f9487a2c6fa24ba5a2f0d413194b955472cd153
MarDevp <zarroukimariem01@gmail.com>
2024-02-16 22:14:26 +0100

    Update SocketServeur.java

--- exercice2/SocketServeur.java
+++ exercice2/SocketServeur.java
@@ -3,28 +3,27 @@ import java.net.*;
 
 public class SocketServeur {
     public static void main(String argv[]) {
-        int port = 10000; // Port d'écoute fixé à 10000
+        int port = 10000; 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Serveur en attente sur le port " + port);
             Socket socket = serverSocket.accept();
             System.out.println("Connexion acceptée depuis " + socket.getInetAddress());
 
-            // Création des flux d'entrée et de sortie
+          
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 
-            // Lecture de l'objet Voiture envoyé par le client
             Voiture voiture = (Voiture) input.readObject();
             System.out.println("Voiture reçue : " + voiture.getType() + " " + voiture.getModel());
 
-            // Modification du carburant de la voiture
+            
             voiture.setCarburant(50); // Par exemple, fixé à 50 litres
 
-            // Envoi de la voiture modifiée en réponse
+            
             output.writeObject(voiture);
 
-            // Fermeture des flux et de la connexion
+          
             output.close();
             input.close();
             socket.close();
