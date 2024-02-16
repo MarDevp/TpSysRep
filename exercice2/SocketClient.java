a7e007f9c1e9d7b5241a2881b7b0df75dbf8ad83
MarDevp <zarroukimariem01@gmail.com>
2024-02-16 22:15:08 +0100

    Update SocketClient.java

--- exercice2/SocketClient.java
+++ exercice2/SocketClient.java
@@ -3,26 +3,24 @@ import java.net.*;
 
 public class SocketClient {
     public static void main(String argv[]) {
-        String host = "localhost"; // Adresse du serveur local
-        int port = 10000; // Port d'écoute du serveur
+        String host = "localhost"; 
+        int port = 10000; 
 
         try {
-            // Connexion au serveur
+          
             Socket socket = new Socket(host, port);
 
-            // Création des flux d'entrée et de sortie
+      
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 
-            // Création d'une voiture et envoi au serveur
+        
             Voiture voiture = new Voiture("SUV", "Toyota");
             output.writeObject(voiture);
 
-            // Réception de la voiture modifiée depuis le serveur
             Voiture voitureModifiee = (Voiture) input.readObject();
             System.out.println("Carburant de la voiture : " + voitureModifiee.getCarburant() + " litres");
 
-            // Fermeture des flux et de la connexion
             output.close();
             input.close();
             socket.close();
